84f1873670f61838e80e7934bd6f1c1011d23c0d
John Brilla <jbrilla17@gmail.com>
2016-02-11 01:51:25 +1100

    adding file processing functionality to watch.sh

--- WatchAndUpload/fswatch_script.sh
+++ WatchAndUpload/fswatch_script.sh
@@ -2,4 +2,10 @@
 # Use full path 
 # make sure script is executable - chmod +x watch.sh
 
-fswatch -o ~/Folder/to/Watch/ | xargs -n1 ~/Script_to_Run/watch.sh
+#fswatch -o ~/Folder/to/Watch/ | xargs -n1 ~/Script_to_Run/watch.sh
+
+echo 'Starting fswatch'
+echo '================'
+echo ''
+
+fswatch -o ~/Desktop/MusicUpload | xargs -n1 ~//Projects/ShellScripts/WatchAndUpload/watch.sh
--- WatchAndUpload/watch.sh
+++ WatchAndUpload/watch.sh
@@ -1,2 +1,29 @@
-echo 'file added...'
+#!/bin/bash
+# This script uses FFMPEG
 
+
+# !!!Execute when COMPLETE FILE is in folder
+
+
+watch_folder="~/Desktop/MusicUpload"
+
+
+echo 'file added...'
+printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+echo 'converting file...'
+printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+
+# For .wav files - convert to 192k mp3
+# Use 
+find ~/Desktop/MusicUpload -name '*.wav' -exec bash -c 'ffmpeg -i $0 -ab 192k ${0/wav/mp3}' {} \;
+
+
+echo 'file(s) converted'
+printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+
+echo 'removing wav...'
+#find ~/Desktop/MusicUpload -name '*.wav' -exec bash -c 'rm $0' {} \;
+
+# Upload file to the internets
+printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+echo 'done'
