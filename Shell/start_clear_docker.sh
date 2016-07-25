519e51153f1b3894726eff8f81abcd6e1afbc7a9
John Brilla <jbrilla17@gmail.com>
2016-07-25 10:24:54 +1000

    Update start_clear_docker.sh

--- Shell/start_clear_docker.sh
+++ Shell/start_clear_docker.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 # starts up docker container / clears logs / starts monitoring
-
-# start container
-docker start $1
+# may need sudo
 
 # Clear Logs
 rm $(docker inspect $1 | grep -G '"LogPath": "*"' | sed -e 's/.*"LogPath": "//g' | sed -e 's/",//g');
+
+# start container
+docker start $1
 
 # Start logs
 docker logs -f $1
